c279bad94491b58556f7d501aff28a2a4f786a57
ebenezer1351 <ebenezer1351@gmail.com>
2024-12-03 15:15:40 -0500

    Add files via upload
    Milestone 5

---
+++ Milestone5 (1).R
@@ -0,0 +1,41 @@
+
+
+
+
+
+
+
+
+
+# Mean trimmed for number of death
+mean(Causes2$Deaths,trim = 0.1)
+# Median
+median(Causes2$Deaths)
+# Variance
+var(Causes2$Deaths)
+#Standard deviation
+sd(Causes2$Deaths)
+
+
+# Mean trimmed for death rate
+mean(Causes2$Deth_Rate,trim = 0.1)
+# Median
+median(Causes2$Death_Rate)
+# Variance
+var(Causes2$Death_Rate)
+#Standard deviation
+sd(Causes2$Death_Rate)
+
+
+
+
+# Mean trimmed for age adjusted Deat rate
+mean(Causes4$Age_Adjusted_Death_Ratee,trim = 0.1)
+# Median
+median(Causes4$Age_Adjusted_Death_Rate)
+# Variance
+var(Causes4$Age_Adjusted_Death_Rate)
+#Standard deviation
+sd(Causes4$Age_Adjusted_Death_Rate)
+
+
